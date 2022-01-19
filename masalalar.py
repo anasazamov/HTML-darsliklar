898635776187936f4832f27a921b3c6dcc148298
anasazamov <95246103+anasazamov@users.noreply.github.com>
2022-01-19 12:25:26 +0500

    Add files via upload

---
+++ masalalar.py
@@ -0,0 +1,8 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+
+logging.debug("dastur haqida ma'lumot")
+
+logging.error("Xatolik haqida ma'lumot")
+
+logging.info("dastur haqida ma'lumot")
