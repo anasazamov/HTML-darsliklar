8cd1e60221a3fba118dd137b57442ac692b14c1f
anasazamov <95246103+anasazamov@users.noreply.github.com>
2022-10-23 23:19:26 +0500

    Delete masalalar.py

--- masalalar.py
+++
@@ -1,8 +0,0 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-
-logging.debug("dastur haqida ma'lumot")
-
-logging.error("Xatolik haqida ma'lumot")
-
-logging.info("dastur haqida ma'lumot")
